b838cf38a9355a70b9aa80c686c4236e34cc0cd7
Zhuldyzya <starzhuzha@gmail.com>
2025-05-18 23:52:06 +0500

    first-commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import MyName from "../MyName/MyName";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello, React!</h1>
+        <p>"This my first React-project with Vite</p>
+        <MyName />
+        <MyName />
+        <MyName />
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/MyName/MyName.jsx
@@ -0,0 +1,9 @@
+import "./MyName.css";
+
+function MyName() {
+    return (
+        <h2>My name is Yersyn</h2>
+    )
+}
+
+export default MyName;
